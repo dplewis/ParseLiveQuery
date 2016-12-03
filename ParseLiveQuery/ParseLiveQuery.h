4b52333699f116a6e764084062c1b938005e89fd
Ruben Flores <dwlewis@uwm.edu>
2016-12-02 18:12:21 -0600

    Initial Commit

---
+++ ParseLiveQuery/ParseLiveQuery.h
@@ -0,0 +1,19 @@
+//
+//  ParseLiveQuery.h
+//  ParseLiveQuery
+//
+//  Created by Diamond Lewis on 12/2/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParseLiveQuery.
+FOUNDATION_EXPORT double ParseLiveQueryVersionNumber;
+
+//! Project version string for ParseLiveQuery.
+FOUNDATION_EXPORT const unsigned char ParseLiveQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseLiveQuery/PublicHeader.h>
+
+
